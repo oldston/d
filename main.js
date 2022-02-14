9ddb72c19547ddd3e928526bc427ec0a3e63b809
oldston <98948084+oldston@users.noreply.github.com>
2022-02-14 18:37:32 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -3,3 +3,16 @@ let changeYourClass = document.getElementById('unorderLists');
 clickThat.addEventListener('click',()=>{
     changeYourClass.classList.toggle('active')
 })
+let locational = document.getElementById('location');
+function one(){
+    locational.innerHTML='Profile'
+}
+function two(){
+    locational.innerHTML='works'
+}
+function three(){
+    locational.innerHTML='experiance'
+}
+function four(){
+    locational.innerHTML='talk'
+}
