a86a2c680927397cdc06a6b43dfc3be757c3c8d7
oldston <98948084+oldston@users.noreply.github.com>
2022-02-18 00:55:42 +0600

    good now

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ clickThat.addEventListener('click',()=>{
 
 
 // ==========
-// change the position Text
+// change the position Text and toggle tabs
 // ============================
 let locational = document.getElementById('location');
 let home = document.getElementsByClassName('home')[0];
@@ -67,6 +67,14 @@ function four(){
 
 
 // ========================
-// hide/show with nav
+// span animation on click
 // ===============================
+let sp1 = document.getElementById('sp1');
+let sp2 = document.getElementById('sp2');
+let sp3 = document.getElementById('sp3');
+clickThat.addEventListener('click',()=>{
+    sp1.classList.toggle('active');
+    sp2.classList.toggle('active');
+    sp3.classList.toggle('active');
 
+})
